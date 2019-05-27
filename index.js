e0e223a0b897a4e79de014e9c3d02c952422e9b7
chadwyck242 <chadwyck.codeart@gmail.com>
2019-05-27 17:25:18 -0600

    Add event listener to keys and console.log operators and operands

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// an object to store the data of our calculator
 const calculator = {
     displayValue: '0',
     firstOperand: null,
@@ -5,9 +6,31 @@ const calculator = {
     operator: null
 }
 
+// a function that handles digits to be output in the display screen
 function updateDisplay() {
     const display = document.querySelector('.calculator-screen');
     display.value = calculator.displayValue;
 }
 
+// initial call to the display function
 updateDisplay();
+
+// adds event listener to handle key press operations
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click', (event) => {
+    // object destructuring; extract the target property from the even object
+    const { target } = event;
+    if (!target.matches('button')) {
+        return;
+    }
+    if (target.classList.contains('operator')) {
+        console.log('operator', target.value);
+        return;
+    }
+    if (target.classList.contains('all-clear')) {
+        console.log('clear', target.value);
+        return;
+    }
+
+    console.log('digit', target.value);
+});
