89fadbd4a33d218ea73f352a8ace8c9f91b55d62
chadwyck242 <chadwyck.codeart@gmail.com>
2019-05-27 18:56:20 -0600

    Create inputDigit() function to allow the entering of digits by the user which will be displayed on the screen

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 // an object to store the data of our calculator
 const calculator = {
-    displayValue: '0',
-    firstOperand: null,
-    awaitSecondOperand: false,
-    operator: null
+	displayValue: '0',
+	firstOperand: null,
+	awaitSecondOperand: false,
+	operator: null
+};
+
+// a function that handles the digits input by the user for display
+// and arithmetic operations
+function inputDigit(digit) {
+	const { displayValue } = calculator;
+	// if screen value is '0', render typed value, otherwise append to current screen value
+	calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
 }
 
 // a function that handles digits to be output in the display screen
 function updateDisplay() {
-    const display = document.querySelector('.calculator-screen');
-    display.value = calculator.displayValue;
+	const display = document.querySelector('.calculator-screen');
+	display.value = calculator.displayValue;
 }
 
 // initial call to the display function
@@ -17,20 +25,21 @@ updateDisplay();
 
 // adds event listener to handle key press operations
 const keys = document.querySelector('.calculator-keys');
-keys.addEventListener('click', (event) => {
-    // object destructuring; extract the target property from the even object
-    const { target } = event;
-    if (!target.matches('button')) {
-        return;
-    }
-    if (target.classList.contains('operator')) {
-        console.log('operator', target.value);
-        return;
-    }
-    if (target.classList.contains('all-clear')) {
-        console.log('clear', target.value);
-        return;
-    }
+keys.addEventListener('click', event => {
+	// object destructuring; extract the target property from the even object
+	const { target } = event;
+	if (!target.matches('button')) {
+		return;
+	}
+	if (target.classList.contains('operator')) {
+		console.log('operator', target.value);
+		return;
+	}
+	if (target.classList.contains('all-clear')) {
+		console.log('clear', target.value);
+		return;
+	}
 
-    console.log('digit', target.value);
+	inputDigit(target.value);
+	updateDisplay();
 });
